4dd7e0a4142a2d17baa4fdc5d22442c7fbcafa45
Nikolay Nikitin <nikitin.nikolay.v@gmail.com>
2021-09-10 22:17:50 +0300

    Initial Commit

---
+++ Quiz 2021/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz 2021
+//
+//  Created by Николай Никитин on 10.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
